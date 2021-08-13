57a618b8a8c78210990796e70e80576741965cac
R2yH2l <roryhall2003@gmail.com>
2021-08-13 15:44:07 -0500

    added user input and a main loop

--- spelling bee solver/Source.cpp
+++ spelling bee solver/Source.cpp
@@ -14,54 +14,60 @@ int main() {
 		strfile.append(line.append(","));
 	}
 	file.close();
-	// parse file
-	std::stringstream ss(strfile);
-	std::string cell{};
-	constexpr const char* ltrs = "ilvteum";
-	int count{};
-	while (std::getline(ss, cell, ',')) {
-		// check word length
-		if (cell.length() >= 4) {
-			bool req = false;
-			// check if word starts with one of the letters
-			for (std::size_t c = 0; c < 7; c++) {
-				if (cell[0] == ltrs[c]) req = true;
-			}
-			// check for requried letter
-			if (req) {
-				for (std::size_t c{}; c < cell.length(); c++) {
-					if (cell[c] == ltrs[0]) {
-						req = true;
-						break;
-					}
-					else req = false;
+	// main loop
+	while (true) {
+		// user input
+		std::string ltrs;
+		std::cout << "letters: ";
+		std::getline(std::cin, ltrs);
+		// parse file
+		std::stringstream ss(strfile);
+		std::string cell{};
+		int count{};
+		while (std::getline(ss, cell, ',')) {
+			// check word length
+			if (cell.length() >= 4) {
+				bool req = false;
+				// check if word starts with one of the letters
+				for (std::size_t c{}; c < ltrs.length(); c++) {
+					if (cell[0] == ltrs[c]) req = true;
 				}
-			}
-			// check word spelling
-			if (req) {
-				bool mis = false;
-				// for each letter check that its from the list
-				for (std::size_t c{}; c < cell.length(); c++) {
-					for (std::size_t t = 0; t < 7; t++) {
-						if (cell[c] == ltrs[t]) {
-							mis = false;
+				// check for requried letter
+				if (req) {
+					for (std::size_t c{}; c < cell.length(); c++) {
+						if (cell[c] == ltrs[0]) {
+							req = true;
 							break;
 						}
-						else mis = true;
-					}
-					if (mis) {
-						break;
+						else req = false;
 					}
 				}
-				// print word
-				if (mis == false) {
-					std::cout << cell << std::endl;
-					count++;
+				// check word spelling
+				if (req) {
+					bool mis = false;
+					// for each letter check that its from the list
+					for (std::size_t c{}; c < cell.length(); c++) {
+						for (std::size_t t{}; t < ltrs.length(); t++) {
+							if (cell[c] == ltrs[t]) {
+								mis = false;
+								break;
+							}
+							else mis = true;
+						}
+						if (mis) {
+							break;
+						}
+					}
+					// print word
+					if (mis == false) {
+						std::cout << cell << std::endl;
+						count++;
+					}
 				}
 			}
 		}
+		// word count
+		std::cout << count << std::endl << std::endl;
 	}
-	// word count
-	std::cout << count << std::endl;
 	return 0;
 }
